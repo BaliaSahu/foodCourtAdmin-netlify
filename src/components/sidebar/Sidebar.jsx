3486dbe8c38c7cd1ebc6a7508a3f50f64b774279
Balia Sahu <baliasahu7382@gmail.com>
2025-11-27 21:15:15 +0530

    updated code

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,25 +1,35 @@
-
 import { Link } from "react-router-dom";
-import logo from "../../assets/software-engineer.png"
+import logo from "../../assets/software-engineer.png";
 
-const Sidebar = ({sidebarVisible}) => {
-    return (
-        sidebarVisible && <div className="border-end bg-white" id="sidebar-wrapper">
-            <div className="sidebar-heading border-bottom bg-light">
-                <img style={{height:"40px",paddingRight:"10px"}} src={logo} alt="" />
-                 Admin</div>
-            <div className="list-group list-group-flush">
-                <Link className="list-group-item list-group-item-action list-group-item-light p-3" to="/add">
-                <i className="bi bi-plus-square"></i> Add Food</Link>
-                
-                <Link className="list-group-item list-group-item-action list-group-item-light p-3" to="/list">
-                <i className="bi bi-list-task"></i> List Food</Link>
-                <Link className="list-group-item list-group-item-action list-group-item-light p-3" to="/orders">
-                <i className="bi bi-cart"></i> Orders</Link>
-                <Link className="list-group-item list-group-item-action list-group-item-light p-3" to="/messages">
-                <i className="bi bi-cart"></i> Messages</Link>
-            </div>
-        </div>
-    )
-}
+const Sidebar = ({ sidebarVisible }) => {
+  return (
+    <div
+      className={`border-end bg-white position-fixed h-100 ${
+        sidebarVisible ? "d-block" : "d-none d-md-block"
+      }`}
+      style={{ width: "250px", zIndex: 100 }}
+    >
+      <div className="sidebar-heading border-bottom bg-light p-2 d-flex align-items-center">
+        <img style={{ height: "40px", paddingRight: "10px" }} src={logo} alt="Logo" />
+        Admin
+      </div>
+
+      <div className="list-group list-group-flush">
+        <Link className="list-group-item list-group-item-action p-3" to="/add">
+          Add Food
+        </Link>
+        <Link className="list-group-item list-group-item-action p-3" to="/list">
+          List Food
+        </Link>
+        <Link className="list-group-item list-group-item-action p-3" to="/orders">
+          Orders
+        </Link>
+        <Link className="list-group-item list-group-item-action p-3" to="/messages">
+          Messages
+        </Link>
+      </div>
+    </div>
+  );
+};
+
 export default Sidebar;
